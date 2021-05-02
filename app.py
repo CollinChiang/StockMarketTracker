d2acaf62e4a4135130eb22a65807962bc7591b7c
CollinChiang <redshirtc5@gmail.com>
2021-05-02 17:29:50 -0400

    Coded Register Function.

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ from bs4 import BeautifulSoup
 from functools import wraps
 import json
 from string import ascii_letters, whitespace, punctuation
+from tempfile import mkdtemp
 
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
@@ -56,14 +59,20 @@ def login_required(method):
     return confirmation
 
 
-def validate(str, arr):
-    if str == "":
+def validate(string, arr):
+    if string == "":
         return False
 
-    for char in str:
+    for char in string:
         if char not in arr:
             return False
     return True
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    return response
 
 
 @app.route("/")
@@ -77,7 +86,7 @@ def index():
     for stock in stocks:
         stock_data.append(get_stock_data(stock))
 
-    render_template("index.html", stock_data=stock_data)
+    return render_template("index.html", stock_data=stock_data)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -88,22 +97,48 @@ def login():
         username = request.form.get("username").strip()
         password = request.form.get("password").strip()
 
-        users_query = Users.query.filter_by(username=username, password=generate_password_hash(password))
+        users_query = Users.query.filter_by(username=username)
 
-        if users_query.count() != 1:
-            return render_template("login.html", username=username, password=password, message="Account information is invalid.")
+        if users_query.count() > 0:
+            for user in users_query:
+                if check_password_hash(user.password, password):
+                    session["uuid"] = user.uuid
+                    return redirect("/")
 
-        session["uuid"] = users_query.uuid
-
-        return redirect("/")
+        return render_template("login.html", message="Account information is invalid.")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
     elif request.method == "POST":
-        pass
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
+        retype_password = request.form.get("retype_password").strip()
+
+        if password != retype_password:
+            return render_template("register.html", username=username, password=password, message="The confirmation password does not match your original password.")
+
+        username_isvalid = validate(username, ascii_letters + whitespace)
+        password_isvalid = validate(password, ascii_letters + whitespace + punctuation)
+
+        if not username_isvalid and not password_isvalid:
+            return render_template("register.html", username=username, password=password, message="The username and password contain invalid characters.")
+        elif not username_isvalid:
+            return render_template("register.html", username=username, password=password, message="The username contain invalid characters.")
+        elif not password_isvalid:
+            return render_template("register.html", username=username, password=password, message="The password contain invalid characters.")
+
+        if Users.query.filter_by(username=username).count() != 0:
+            return render_template("register.html", username=username, password=password, message="The username is already in use.")
+
+        user = Users(username=username, password=generate_password_hash(password), stocks=json.dumps([]))
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect("/login")
+
 
 
 @app.route("/add")
